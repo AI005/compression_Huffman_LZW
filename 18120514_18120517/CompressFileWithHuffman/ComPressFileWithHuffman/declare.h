01753e85b8b459c0facd65602e6ab5e261e537f2
AI005 <50285308+AI005@users.noreply.github.com>
2019-12-15 00:30:19 +0700

    Huffman

---
+++ 18120514_18120517/CompressFileWithHuffman/ComPressFileWithHuffman/declare.h
@@ -0,0 +1,34 @@
+﻿#pragma once
+#include <iostream>
+using namespace std;
+#include <string>
+
+struct NODE
+{
+	unsigned char c;
+	int freq;   // tần số xuất hiện
+	bool used;  // Đánh dấu xem Node đã xử lí hay chưa
+	int nLeft;  // số lượng nút con nằm bên trái
+	int nRight; // số lượng nút con nằm bên phải
+};
+
+struct BITCODE 
+{
+	char* bits;
+	int     soBit;
+};
+
+#define MAX_NODE        256*9
+#define MAX_LENGTH_BIT  10
+class Huffman
+{
+private:
+	NODE huffTree[MAX_NODE];
+	BITCODE TableBitCode[256];
+	unsigned int nNode;
+public:
+	Huffman();
+	~Huffman();
+	void initFrequences(string filename); // thống kê tần số xuất hiện
+
+}
